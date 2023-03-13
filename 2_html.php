11b30a37a80e620e48fc5e3f80216225e80a183e
Bakkonrad <konri0908@gmail.com>
2023-03-13 10:05:52 +0100

    commit

--- 2_html.php
+++ 2_html.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h4>Lista</h4> 
+    <h4>Lista</h4>
     <ul>
         <li>
             Poznań
+            <ol type="A"> <!-- lista numerowana -->
+                <li>Polna</li>
+                <li>Gnieźnieńska</li>
+            </ol>
+        </li>
+        <li>Gniezno</li>
+        <li>Jarocin</li>
+    </ul>
+
+    <?php
+    $city = "Poznań"; // w html nie mogę użyć zmiennej 
+    echo <<<LIST
+        <br>HEREDOC</br>
+        <ul>
+        <li>
+            $city 
             <ol> <!-- lista numerowana -->
                 <li>Polna</li>
                 <li>Gnieźnieńska</li>
@@ -19,5 +37,8 @@
         <li>Gniezno</li>
         <li>Jarocin</li>
     </ul>
+    LIST;
+    ?>
 </body>
+
 </html>
